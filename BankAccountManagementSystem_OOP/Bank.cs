ecf1fb468dd9c78f42287b1e19014d807be7c926
Azzak <azzaalmahrooqi79@gmail.com>
2024-09-25 14:34:59 +0400

    create 2 classes (Bank class - BankAccount class)

---
+++ BankAccountManagementSystem_OOP/Bank.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountManagementSystem_OOP
+{
+    internal class Bank
+    {
+    }
+}
