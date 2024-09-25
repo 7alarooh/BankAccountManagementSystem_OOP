e8e02d31e8d36ee7e0db0f738abba4b406569acb
Azzak <azzaalmahrooqi79@gmail.com>
2024-09-25 15:17:35 +0400

    add two constructors; constructor with two parameters, and second constructor that includes an initial deposit amount along with the accountHolderName and accountNumber.

--- BankAccountManagementSystem_OOP/BankAccount.cs
+++ BankAccountManagementSystem_OOP/BankAccount.cs
@@ -11,8 +11,30 @@ namespace BankAccountManagementSystem_OOP
         // private properties
         private string accountNumber;
         private string accountHolderName;
-        private double balance;
+        private decimal balance;
 
+        // constructor with 2 parameters
+        public BankAccount(string accountHolderName, string accountNumber) 
+        {
+            this.accountHolderName = accountHolderName;
+            this.accountNumber = accountNumber;
+            this.balance = 0;//default balance
+
+        }
+        // constructor with an initial deposit amount
+        public BankAccount(string accountHolderName, string accountNumber,decimal initialDeposit )
+        { 
+            this.accountHolderName = accountHolderName;
+            this.accountNumber = accountNumber;
+
+            if (initialDeposit > 0) { balance = initialDeposit;
+                Console.WriteLine($"Successfully deposited {initialDeposit} OMR");
+            }
+            else {
+                Console.WriteLine("deposit amount must be positive!");
+            }
+
+        }
 
 
     }
